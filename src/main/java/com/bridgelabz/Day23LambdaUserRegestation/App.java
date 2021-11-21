b8b3c0048ffd603ca0ff5080a3b8a1bb52592be7
Naveen Pattanashetti <pattanashettinaveen.com>
2021-11-21 13:18:02 +0530

    Added:Wel Come To User

---
+++ src/main/java/com/bridgelabz/Day23LambdaUserRegestation/App.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.Day23LambdaUserRegestation;
+
+public class App {
+	public static void main(String[] args) {
+		System.out.println("wel come to lambda user regestation Program");
+	}
+}
